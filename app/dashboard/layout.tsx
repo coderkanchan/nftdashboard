601391754b4531fdfc90b783bf4be46f70add5a1
kanchan sharma <kanusharma881388@gmail.com>
2026-01-10 22:28:39 +0530

    Fix: audit log button ui and nextauth url and google login

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,22 +1,3 @@
-// import Sidebar from "@/components/Sidebar";
-// import Navbar from "@/components/Navbar";
-
-// export default function DashboardLayout({children,}: {children: React.ReactNode;}) {
-//   return (
-//     <div className="flex min-h-screen bg-[#0c0c0e]">
-//       <Sidebar />
-
-//       <div className="flex-1 flex flex-col">
-//         <Navbar />
-//         <main className="p-6">{children}</main>
-//       </div>
-//     </div>
-//   );
-// }
-
-
-
-
 "use client";
 
 import Sidebar from "@/components/Sidebar";
